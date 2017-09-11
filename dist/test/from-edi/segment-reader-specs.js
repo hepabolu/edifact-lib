ab08563563c41874936865625e65e0b00f895786
Helma van der Linden <hepabolu@gmail.com>
2017-09-11 23:42:40 +0200

    test to handle UNA segment

--- dist/test/from-edi/segment-reader-specs.js
+++ dist/test/from-edi/segment-reader-specs.js
@@ -60,6 +60,13 @@ suite('Segment Reader', function () {
     assert.deepEqual(reader.currentDataElement(), expected);
   });
 
+  test('should allow escape char to be escaped with an UNA segment', function () {
+    var input = "UNA:+.? 'BGM+ABC:Some?:doc?+code??:BCD:Some Name+di-1234:1.0.1:234+9+CBA+dsc+eng";
+    var reader = new Edi.EdiSegmentReader(input, ediConfig);
+    var expected = ['ABC', 'Some:doc+code?', 'BCD', 'Some Name'];
+    assert.deepEqual(reader.currentDataElement(), expected);
+  });
+
   test('should allow escaped data components', function () {
     var reader = new Edi.EdiSegmentReader(input, ediConfig);
     var actual = reader.current();
